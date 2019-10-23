92a4374494149244f3c4981d90304bb5c50dd6c3
Daniel Otieno <danielotieno.ke@gmail.com>
2019-10-23 10:09:24 +0300

    update database connection

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import connectDB from './config/db'
 const app = express()
 
 // Connect Database
-
 connectDB()
 
 app.get('/', (req, res) => {
@@ -16,7 +15,7 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/users', require('./routes/users'))
 app.use('/api/contacts', require('./routes/contacts'))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`)
