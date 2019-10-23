e73bd6ba6cae232a6148da76558e480f2c367562
Daniel Otieno <danielotieno.ke@gmail.com>
2019-10-23 08:23:49 +0300

    create a database connection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import connectDB from './config/db'
 
 const app = express()
+
+// Connect Database
+
+connectDB()
 
 app.get('/', (req, res) => {
   res.json({ meaage: 'Welcome to contact manager API...' })
