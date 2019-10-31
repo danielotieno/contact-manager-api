4066f7b460b054884ef920d5f011b7f0946cab97
Daniel Otieno <danielotieno.ke@gmail.com>
2019-10-31 13:21:02 +0300

    create contact model

---
+++ models/Contact.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  type: {
+    type: String,
+    default: 'Personal',
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+})
+
+module.exports = mongoose.model('contact', ContactSchema)
