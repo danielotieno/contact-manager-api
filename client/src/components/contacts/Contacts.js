e0af4a6607f8f71844cac68c12380d2e51b1d8b4
Daniel Otieno <danielotieno.ke@gmail.com>
2019-11-12 16:40:35 +0300

    create contacts component

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import ContactContext from "../../context/contact/contactContext";
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts } = contactContext;
+  return (
+    <>
+      {contacts.map(contact => (
+        <h3>{contact.name}</h3>
+      ))}
+    </>
+  );
+};
+
+export default Contacts;
