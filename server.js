72e37fb5f11354e1eb7a8e897dc228c194c7af95
Daniel Otieno <danielotieno.ke@gmail.com>
2020-02-16 14:52:57 +0300

    chore:update scripts

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 6500;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
